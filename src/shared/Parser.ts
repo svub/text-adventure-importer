031e44e364e73284500c7f7e3092abd4d5c88c23
Sven <sven@nimiq.com>
2020-11-25 20:25:15 -0600

    rough first version

---
+++ src/shared/Parser.ts
@@ -0,0 +1,189 @@
+import { Book, Section, Chapter, ElementType, Paragraph, If, Else, HasElements, Link } from './entities';
+import { Token, TokenType } from './Lexer';
+import { RemoveItem, State, ChangeState, AddItem } from '../../../app/src/shared/entities';
+
+export enum CommandType {
+  book = 'book',
+  chapter = 'chapter',
+  section = 'section',
+  if = 'if',
+  else = 'else',
+  endif = 'endif',
+  item = 'item',
+  state = 'state',
+  next = '>',
+  jump = '>>',
+}
+
+export type Command = {
+  type: string;
+  fields: string[];
+  line: number;
+};
+
+export default class Parser {
+  position = 0;
+
+  reset() {
+    this.position = 0;
+  }
+
+  parse(tokens: Token[]): Book {
+    let book: Book | null = null;
+    let chapter: Chapter | null = null;
+    let section: Section | null = null;
+    let ifElement: If | null = null;
+    let elseElement: Else | null = null;
+    const getTitle = (command: Command): string => {
+      const next = tokens[this.position + 1];
+      if (next.type !== TokenType.paragraph) {
+        this.error('Book needs a title as text after // book command', command, next);
+      }
+      this.position++;
+      return next.data;
+    }
+    const topContainer = (command?: Command): HasElements => {
+      const element = ([elseElement, ifElement, section].find(e => e !== null));
+      if (!element) {
+        if (command)
+          this.error(`Found a "// ${command.type}" before first "// section"`, command);
+        else
+          this.error('Found text before first "// section"');
+      }
+      return element!;
+    }
+
+    do {
+      const token = tokens[this.position];
+
+      if (token.type === TokenType.command) {
+        const command = this.parseCommand(token);
+        switch (command.type) {
+          case CommandType.book:
+            if (book) this.error('Found a second "// book" command. Book already initialize', book, command);
+            book = {
+              title: getTitle(command),
+              chapters: [],
+            }
+            break;
+          case CommandType.chapter:
+            if (!book) this.error('Found a "// chapter" before "// book"', command);
+            chapter = {
+              id: command.fields[0],
+              title: getTitle(command),
+              sections: [],
+            };
+            book!.chapters.push(chapter);
+            break;
+          case CommandType.section:
+            if (!chapter) this.error('Found a "// section" before first "// chapter"', command);
+            section = {
+              id: command.fields[0],
+              title: getTitle(command),
+              elements: [],
+              next: [],
+            };
+            chapter!.sections.push(section);
+            break;
+          case CommandType.next:
+            if (!chapter) this.error('Found a "// >" before first "// chapter"', command);
+            if (!section) this.error('Found a "// >" before first "// section"', command);
+            const link: Link = {
+              title: command.fields.slice(1).join(' '),
+              sectionId: command.fields[0],
+              chapterId: chapter!.id, // same chapter link
+            };
+            // TODO add link related commands
+            section!.next.push(link);
+            break;
+          case CommandType.jump:
+            if (!section) this.error('Found a "// >" before first "// section"', command);
+            const jump: Link = {
+              title: command.fields.slice(2).join(' '),
+              sectionId: command.fields[0],
+              chapterId: command.fields[1], // cross chapter link
+            };
+            // TODO add related commands
+            section!.next.push(jump);
+            break;
+          case CommandType.state:
+            const state: ChangeState = {
+              type: ElementType.state,
+              id: command.fields[0],
+              modifier: command.fields[1],
+            };
+            topContainer(command).elements.push(state);
+            break;
+          case CommandType.item:
+            if (!section) this.error('Found a "// item" before first "// section"', command);
+            const item = (command.fields[0].toLocaleLowerCase() === 'remove')
+              ? {
+                type: ElementType.removeItem,
+                id: command.fields[1],
+              } as RemoveItem
+              : {
+                type: ElementType.addItem,
+                id: command.fields[0],
+              } as AddItem;
+            topContainer(command).elements.push(item);
+            break;
+          case CommandType.if:
+            if (ifElement) this.error('Found another "// if" before "// end if"', command);
+            ifElement = {
+              type: ElementType.if,
+              condition: command.fields.join(' '),
+              elements: [],
+            };
+            section!.elements.push(ifElement);
+            break;
+          case CommandType.else:
+            if (!ifElement) this.error('Found "// else" before "// if"', command);
+            elseElement = {
+              type: ElementType.else,
+              ifCondition: ifElement!.condition,
+              elements: [],
+            };
+            section!.elements.push(elseElement);
+            break;
+          case CommandType.endif:
+            if (!ifElement) this.error('Found "// endif" before "// if"', command);
+            ifElement = elseElement = null;
+            break;
+          default:
+            this.error(`Command type ${command.type} not implemented`, command);
+          }
+      } else { // text
+        if (!section) this.error('Found a text before first "// section"', token);
+        const element: Paragraph = {
+          type: ElementType.paragraph,
+          text: token.data,
+        };
+        topContainer().elements.push(element);
+      }
+
+      this.position++;
+      // if (this.position >= tokens.length) break; // done
+    } while (this.position < tokens.length);
+    if (!book) {
+      this.error('// book not found');
+    }
+    return book!;
+  }
+
+  parseCommand(token: Token): Command {
+    if (token.type != TokenType.command) {
+      this.error('Cannot parse command, token is not a command', token);
+    }
+    const elements = token.data.split(' ').map(s => s.trim()).filter(s => s.length > 0);
+    return {
+      type: elements[0].toLocaleLowerCase(),
+      fields: elements.slice(1),
+      line: token.line,
+    }
+  }
+
+  error(message: string, ...data: any[]) {
+    console.error(message, ...data);
+    throw new Error(`${message}: ${data}`);
+  }
+}
---
+++ src/views/Sources.vue
@@ -0,0 +1,128 @@
+<template lang="pug">
+main.sources
+  textarea.urls(ref="urls")
+    | https://docs.google.com/document/d/e/2PACX-1vRvBU8wGqFOEdnXAwQq618pk3oLmdofNJj2f4AvNmyl6wzz7vspLekZF95G_n2iXtPIjP6yMcHs4rw9/pub
+    | https://docs.google.com/document/d/e/2PACX-1vQ_56Jw7HG3h6HuJ9q50w-wqyasNixc1o9yyiOhxjAnHtmgl53aKi1Z5WWZR1JdfOEXhkajBhvD2XoK/pub
+  button.load(@click="load") load
+  textarea.text(ref="text")
+  textarea.tokens(ref="tokens")
+  textarea.book(ref="book")
+</template>
+
+<script lang="ts">
+  import { Component, Vue, Prop } from 'vue-property-decorator';
+  import { State, Action } from 'vuex-class';
+  import { loadText, log, warn } from '../shared/util';
+  import { TextEntity, Book } from "../shared/entities";
+  import Lexer from "../shared/Lexer";
+  import Parser from "../shared/Parser";
+
+  const dummy = window.location.href.includes('localhost');
+
+type ParserMessage = {
+    line: number;
+    type: ["error", "warning"];
+    error: any;
+  };
+
+  type Parsed = TextEntity & {
+    end: number;
+  };
+
+  @Component({
+    name: 'Sources',
+  })
+  export default class Sources extends Vue {
+    // const textareas = { urls: ref(null), text: ref(null), tokens: ref(null), book: ref(null) }
+
+    async load() {
+      let book: Book;
+      const urls = dummy
+        ? ['/test-data1.html', '/test-data2.html']
+        : (this.$refs.urls! as HTMLTextAreaElement).value!.split("\n");
+      console.log(urls);
+
+      const text = (await Promise.all(urls.map(async (url) => {
+        console.log(url);
+        const raw = await loadText(url);
+        // HTML?
+        try {
+          if (raw.indexOf('<!DOCTYPE html>') >= 0) {
+            console.log('HTML');
+            const doc = new DOMParser().parseFromString(raw, "text/html").documentElement;
+            // console.log(doc);
+            console.log(doc.querySelector('#footer')!.textContent!);
+            // Google Docs?
+            if ((url.indexOf('docs.google.com/document') >= 0) ||
+              (doc.querySelector('#footer')!.textContent!.indexOf('Published by Google Drive') >= 0)) {
+              console.log('GDocs');
+              const root = doc.querySelector('#contents > div')!;
+              console.log(root);
+              root.innerHTML = root.innerHTML.replace(/<p>|<h[\d]>/gi, "").replace(/<br>|<\/p>|<\/h[\d]>/gi, "\n");
+              return root.textContent;
+            }
+            if (doc.querySelector('body')) {
+              console.log('body');
+              return doc.querySelector('body')!.textContent;
+            }
+            return doc.textContent;
+          }
+        } catch (error) {
+          console.error(`Failed parsing ${url}`, error);
+        }
+        return raw;
+      }))).join("\n");
+      // console.log('text',text);
+      (this.$refs.text! as HTMLTextAreaElement).value = text;
+
+      const lexer = new Lexer();
+      const tokens = lexer.tokenize(text);
+      console.log('tokens', tokens);
+      (this.$refs.tokens! as HTMLTextAreaElement).value = JSON.stringify(tokens, null, ' ');
+      const parser = new Parser();
+      const messages: ParserMessage[] = [];
+      try {
+        book = parser.parse(tokens);
+        log('load', book);
+        (this.$refs.book! as HTMLTextAreaElement).value = JSON.stringify(book, null, ' ');
+      } catch (parserMessages) {
+        messages.push(...parserMessages);
+      }
+
+      if (messages.length > 0)
+        warn("Errors", messages);
+      else
+        log("Parsing successful.");
+    }
+
+    test() {
+      // each book needs 1+ chapters
+      // each chapter needs 1+ sections
+      // each if needs 1+ elements
+      // each else needs 1+ elements
+      // check that all formatting options are closed for each paragraph
+      // check that each link has a target
+      // check that each section is linked to
+      // check if all items of addItem exist
+      // check if items exist that are not in an addItem
+      // check if all items of removeItem exist
+      // check if items exist that are not in a removeItem
+      // check if removeItem exist that have not been addItem before
+    }
+  }
+
+</script>
+
+<style lang="stylus">
+main.sources
+  display flex
+  flex-direction column
+  margin auto
+  max-width 70em
+
+  textarea
+    height 10em
+
+    &.book
+      height 30em
+</style>
