f53560842d8f77686a5a3f5e232e860b24ad4b77
Sven <sven@nimiq.com>
2020-12-22 19:54:11 +0100

    ignore long lines

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,13 @@ module.exports = {
     ecmaVersion: 2020
   },
   rules: {
+    "max-len": ["error", 120, 2, {
+      ignoreUrls: true,
+      ignoreComments: false,
+      ignoreRegExpLiterals: true,
+      ignoreStrings: false,
+      ignoreTemplateLiterals: false,
+    }],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-case-declarations': 'off',
